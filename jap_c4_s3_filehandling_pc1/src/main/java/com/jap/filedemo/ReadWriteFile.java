ce9f350d8baa75cb0b155a836b95eb99454dd14e
Rameshwarjha <jharameshwar.111@gmail.com>
2022-03-19 11:01:31 +0530

    changes

---
+++ jap_c4_s3_filehandling_pc1/src/main/java/com/jap/filedemo/ReadWriteFile.java
@@ -0,0 +1,27 @@
+package com.jap.filedemo;
+
+import java.io.*;
+
+public class ReadWriteFile {
+
+    public static void main(String[] args) {
+   ReadWriteFile readWriteFile = new ReadWriteFile();
+        try {
+            readWriteFile.readDataFromFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //create object of Readers and writers
+    public void readDataFromFile() throws IOException {
+
+		
+
+
+
+
+
+    }
+}
